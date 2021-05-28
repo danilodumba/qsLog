ffb95fa5d00c5a61d7ec17428f7aeccc0d1398de
Danilo <danilodumba@yahoo.com.br>
2021-05-28 10:37:19 -0300

    Testes no healtcheck

--- qslog-back/src/qsLog.Api/Startup.cs
+++ qslog-back/src/qsLog.Api/Startup.cs
@@ -75,7 +75,7 @@ namespace qsLog
             }
             
             this.ConfigureJWT(services);
-            this.ConfigureHealthChecks(services);
+            //this.ConfigureHealthChecks(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
